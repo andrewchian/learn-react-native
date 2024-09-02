5aad3816635b546bd8ef2f73263e75f093d2e2c7
AndrewChian <andrew.chian@gmail.com>
2024-09-02 17:32:33 -0400

    seperated into 2 columns using flatlist to display all the pizzas

--- FoodOrdering/src/app/(tabs)/index.tsx
+++ FoodOrdering/src/app/(tabs)/index.tsx
@@ -1,14 +1,19 @@
-import { View } from '@/src/components/Themed';
+import { View, } from '@/src/components/Themed';
 import products from '@/assets/data/products';
 import ProductListItem from '@/src/components/ProductListItem';
+import { FlatList } from 'react-native';
 
 
 export default function MenuScreen() {
   return (
     <View>
-      <ProductListItem product={products[5]} />
-      <ProductListItem product={products[1]} />
-
+      <FlatList
+        data={products}
+        renderItem={({ item }) => <ProductListItem product={item}/>}
+        numColumns={2}
+        contentContainerStyle={{gap:10, padding:10}}
+        columnWrapperStyle={{gap:10}}
+      />
     </View>
   );
 }
